9c96b2a6194c0b6d456124ec6c155f3aec3465a9
robertjaskowski <robertjanjaskowski@gmail.com>
2021-03-30 10:24:46 +0200

    updating fixing

--- LauncherCaravansary/Form1.Designer.cs
+++ LauncherCaravansary/Form1.Designer.cs
@@ -29,35 +29,78 @@ namespace LauncherCaravansary
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.topLabel = new System.Windows.Forms.Label();
+            this.progressText = new System.Windows.Forms.Label();
+            this.retryButton = new System.Windows.Forms.Button();
+            this.closeButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // label1
+            // topLabel
             // 
-            this.label1.AutoSize = true;
-            this.label1.ForeColor = System.Drawing.Color.White;
-            this.label1.Location = new System.Drawing.Point(2, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(183, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Updating Caravansary. Wait a minute";
+            this.topLabel.AutoSize = true;
+            this.topLabel.ForeColor = System.Drawing.Color.White;
+            this.topLabel.Location = new System.Drawing.Point(12, 9);
+            this.topLabel.Name = "topLabel";
+            this.topLabel.Size = new System.Drawing.Size(149, 13);
+            this.topLabel.TabIndex = 0;
+            this.topLabel.Text = "Updating Caravansary. Wait...";
+            // 
+            // progressText
+            // 
+            this.progressText.AutoSize = true;
+            this.progressText.ForeColor = System.Drawing.Color.White;
+            this.progressText.Location = new System.Drawing.Point(12, 39);
+            this.progressText.Name = "progressText";
+            this.progressText.Size = new System.Drawing.Size(0, 13);
+            this.progressText.TabIndex = 1;
+            // 
+            // retryButton
+            // 
+            this.retryButton.Location = new System.Drawing.Point(15, 62);
+            this.retryButton.Name = "retryButton";
+            this.retryButton.Size = new System.Drawing.Size(75, 23);
+            this.retryButton.TabIndex = 2;
+            this.retryButton.Text = "Retry";
+            this.retryButton.UseVisualStyleBackColor = true;
+            this.retryButton.Visible = false;
+            this.retryButton.Click += new System.EventHandler(this.retryButton_Click);
+            // 
+            // closeButton
+            // 
+            this.closeButton.Location = new System.Drawing.Point(96, 62);
+            this.closeButton.Name = "closeButton";
+            this.closeButton.Size = new System.Drawing.Size(75, 23);
+            this.closeButton.TabIndex = 3;
+            this.closeButton.Text = "Close";
+            this.closeButton.UseVisualStyleBackColor = true;
+            this.closeButton.Visible = false;
+            this.closeButton.Click += new System.EventHandler(this.closeButton_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(20)))), ((int)(((byte)(20)))), ((int)(((byte)(20)))));
-            this.ClientSize = new System.Drawing.Size(184, 61);
-            this.Controls.Add(this.label1);
+            this.ClientSize = new System.Drawing.Size(184, 97);
+            this.Controls.Add(this.closeButton);
+            this.Controls.Add(this.retryButton);
+            this.Controls.Add(this.progressText);
+            this.Controls.Add(this.topLabel);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "Form1";
             this.ShowIcon = false;
             this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
+            this.TopMost = true;
             this.TransparencyKey = System.Drawing.Color.White;
             this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Shown += new System.EventHandler(this.Form1_Shown);
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -65,7 +108,10 @@ namespace LauncherCaravansary
 
         #endregion
 
-        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label topLabel;
+        private System.Windows.Forms.Label progressText;
+        private System.Windows.Forms.Button retryButton;
+        private System.Windows.Forms.Button closeButton;
     }
 }
 
--- LauncherCaravansary/Form1.cs
+++ LauncherCaravansary/Form1.cs
@@ -1,11 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.IO.Compression;
+using System.Linq;
 using System.Net;
 using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Windows.Threading;
 
 namespace LauncherCaravansary
 {
@@ -14,7 +18,7 @@ namespace LauncherCaravansary
 
 
         static string appdataAPPDATA_PATH = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
-        static string appdataCFOLDER_PATH = Path.Combine(appdataAPPDATA_PATH, "Caravansary");
+        static string appdataCFOLDER_PATH = Path.Combine(appdataAPPDATA_PATH, "Caravansary" + Path.DirectorySeparatorChar);
 
 
         static string dumpFileLocation = Path.Combine(appdataCFOLDER_PATH, "dump.txt");
@@ -23,8 +27,14 @@ namespace LauncherCaravansary
 
         string launcherApplicationPath = System.Reflection.Assembly.GetExecutingAssembly().Location;
         static string launcherApplicationDirectoryPath = Directory.GetParent(Assembly.GetExecutingAssembly().Location).ToString();
-        
 
+
+        static string AppdataCLauncherPathZip = appdataCFOLDER_PATH + Path.DirectorySeparatorChar + "LauncherCaravansary.zip";
+        static string AppdataCLauncherPathExe = appdataCFOLDER_PATH + Path.DirectorySeparatorChar + "LauncherCaravansary.exe";
+
+        static string appdataAppPath = appdataCFOLDER_PATH + Path.DirectorySeparatorChar + "Caravansary.exe";
+
+        static string appdataZipPath = appdataCFOLDER_PATH + "x64Caravansary.zip";
 
         [DllImport("user32.dll", CharSet = CharSet.Auto)]
         private static extern IntPtr SendMessage(IntPtr hWnd, UInt32 Msg, IntPtr wParam, IntPtr lParam);
@@ -36,97 +46,328 @@ namespace LauncherCaravansary
             SendMessage(hwnd, WM_CLOSE, IntPtr.Zero, IntPtr.Zero);
         }
 
+        List<string> args;
 
+        List<string> returnedArgs;
+
+        private bool ArgsContain(string str, out string result)
+        {
+
+
+            for (int i = args.Count - 1; i >= 0; i--)
+            {
+                args[i] = args[i].ToLower().Trim();
+
+
+                if (args[i].Contains(str.ToLower().Trim()))
+                {
+                    result = args[i];
+                    return true;
+                }
+            }
+
+
+            result = null;
+            return false;
+        }
+
+        private bool ArgsContain(string str, string withoutString, out string result)
+        {
+
+
+            for (int i = args.Count - 1; i >= 0; i--)
+            {
+                args[i] = args[i].ToLower().Trim();
+
+                if (args[i].Contains(withoutString.ToLower().Trim()))
+                {
+                    continue;
+                }
+                if (args[i].Contains(str.ToLower().Trim()))
+                {
+                    result = args[i];
+                    return true;
+                }
+            }
+
+
+            result = null;
+            return false;
+        }
+
+        string _mainAppLocationDir;
+        string GetMainAppLocationDir()
+        {
+            if (_mainAppLocationDir == null)
+            {
+
+                bool hasLocationArg = ArgsContain("Caravansary", "LauncherCaravansary", out string locationArg);
+
+                if (hasLocationArg)
+                {
+                    _mainAppLocationDir = locationArg;
+                }
+
+                if (File.Exists(appdataAppPath))
+                {
+                    _mainAppLocationDir = Directory.GetParent(appdataAppPath).FullName;
+                }
+
+                _mainAppLocationDir = @".\";
+            }
+
+
+            return _mainAppLocationDir;
+        }
+
+        string _mainAppLocationExe;
+        string GetMainAppLocationExe()
+        {
+            if (_mainAppLocationExe == null) _mainAppLocationExe = GetMainAppLocationDir() + Path.DirectorySeparatorChar + "Caravansary.exe";
+            return _mainAppLocationExe;
+        }
+
+        private void StartMainApp()
+        {
+            string stringArg = "";
+
+            foreach (var item in returnedArgs)
+            {
+                stringArg = item + " ";
+            }
+
+
+            Process.Start(GetMainAppLocationExe(), stringArg);
+            this.Close();
+
+        }
 
         public Form1()
         {
             InitializeComponent();
 
-            Debug.WriteLine(appdataAPPDATA_PATH);
-            Debug.WriteLine(launcherApplicationPath);
-            Debug.WriteLine(launcherApplicationDirectoryPath);
-            var client = new WebClient();
+        }
 
-            try
+
+        private async void Form1_Shown(object sender, EventArgs e)
+        {
+            args = Environment.GetCommandLineArgs().ToList();
+            returnedArgs = new List<string>();
+            returnedArgs.Add("StartedThroughLauncher");
+
+            this.topLabel.Text = "Launching caravansary...";
+
+            //debug label
+            args.Add("RequstedUpdate");
+
+            if (args.Contains("RequstedUpdate"))
             {
-                System.Threading.Thread.Sleep(5000);
 
+                bool res = await UpdateMainApp();
+                if (!res) return;
 
+                returnedArgs.Add("Updated");
+                StartMainApp();
 
-
-                if (File.Exists(dumpFileLocation))
+            }
+            else
+            {
+                var appPath = GetMainAppLocationExe();
+                if (!File.Exists(appPath))
                 {
-                    string interlophandle = File.ReadAllText(dumpFileLocation);
-                    if (!string.IsNullOrEmpty(interlophandle))
-                    {
-                        if (int.TryParse(interlophandle, out int result))
-                        {
-                            if (result > 0)
-                                CloseWindow((IntPtr)result);
-                        }
-                    }
+                    await UpdateMainApp();
+                    returnedArgs.Add("DownloadedEXEFromLauncherToAppdata");
+                    StartMainApp();
+                }
+                else
+                {
+                    StartMainApp();
 
 
-                    //File.Delete(dumpFileLocation);
                 }
 
 
+            }
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
 
 
+        }
+
+
+        private bool DeleteMainApplication()
+        {
+
+
+            int tries = 0;
+
+            while (tries < 20)
+            {
+                if (tries > 0)
+                {
+                    System.Threading.Thread.Sleep(1000);
+                    GC.Collect();
+                    GC.WaitForPendingFinalizers();
+
+                }
 
                 try
                 {
-                    if (File.Exists(@".\Caravansary.exe"))
+                    if (File.Exists(GetMainAppLocationExe()))
                     {
 
-                        File.Delete(@".\Caravansary.exe");
+                        File.Delete(GetMainAppLocationExe());
+                        return true;
                     }
+
+                    return true;
                 }
                 catch
                 {
-                    File.WriteAllText(dumpFileLocation, "Can't delete Caravansary.exe");
-
+                    // File.WriteAllText(dumpFileLocation, "Can't delete Caravansary.exe");
+                    tries++;
+                    UpdateLabelText(progressText, "Deleting main app failed..." + tries + " times");
                 }
 
+            }
 
+            return false;
 
-
-
-
-
-                client.DownloadFile("https://github.com/RobertJaskowski/Caravansary/releases/latest/download/x64Caravansary.zip", @"x64Caravansary.zip");
-                string zipPath = @".\x64Caravansary.zip";
-
-
-
-                string tempExtractingPathToExe = Path.Combine(appdataCFOLDER_PATH, @"Caravansary.exe");
-                string tempExtractingPathToUpdaterExe = Path.Combine(appdataCFOLDER_PATH, @"LauncherCaravansary.exe");
-
-                if (File.Exists(tempExtractingPathToExe))
+        }
+        private async Task<bool> DownloadNewVersion()
+        {
+            try
+            {
+                using (WebClient client = new WebClient())
                 {
-                    File.Delete(tempExtractingPathToExe);
-                    File.Delete(tempExtractingPathToUpdaterExe);
+                    UpdateLabelText(this.progressText, "Downloading...");
+
+                    await Task.Run(() => client.DownloadFile("https://github.com/RobertJaskowski/Caravansary/releases/latest/download/x64Caravansary.zip", appdataZipPath));
+
                 }
 
-                ZipFile.ExtractToDirectory(zipPath, appdataCFOLDER_PATH);
-                //File.Move(tempExtractingPathToExe, applicationPath);
-                //File.Copy("Caravansary.exe", applicationPath, true);
-                FileInfo fi = new FileInfo(tempExtractingPathToExe);
-                fi.CopyTo(Path.Combine(launcherApplicationDirectoryPath, "Caravansary.exe"), true);
-
-
-
-                File.Delete(@".\x64Caravansary.zip");
-                Process.Start(@".\Caravansary.exe");
-                this.Close();
+                return true;
             }
             catch
             {
-                File.WriteAllText(dumpFileLocation, "app update failed");
-                //Debug.WriteLine("app update fail");
-                this.Close();
-
+                return false;
             }
         }
+
+        private async Task<bool> ExtractZip()
+        {
+
+            UpdateLabelText(this.progressText, "Extracting...");
+
+            try
+            {
+
+
+                //string tempExtractingPathToExe = Path.Combine(appdataCFOLDER_PATH, @"Caravansary.exe");
+                //string tempExtractingPathToUpdaterExe = Path.Combine(appdataCFOLDER_PATH, @"LauncherCaravansary.exe");
+
+                //if (File.Exists(tempExtractingPathToExe))
+                //{
+                //    File.Delete(tempExtractingPathToExe);
+                //    File.Delete(tempExtractingPathToUpdaterExe);
+                //}
+
+
+
+                using (var archive = ZipFile.OpenRead(appdataZipPath))
+                {
+
+                    foreach (var entry in archive.Entries)
+                    {
+
+                        entry.ExtractToFile(Path.Combine(GetMainAppLocationDir(), entry.FullName), true);
+                    }
+                }
+
+
+                //ZipFile.ExtractToDirectory(zipPath, appdataCFOLDER_PATH);
+
+                //FileInfo fi = new FileInfo(tempExtractingPathToExe);
+                //fi.CopyTo(GetMainAppLocationDir(), true);
+
+
+
+                File.Delete(appdataZipPath);
+
+
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+
+        }
+
+
+        private async Task<bool> UpdateMainApp()
+        {
+            this.ShowInTaskbar = true;
+            this.WindowState = FormWindowState.Normal;
+            UpdateLabelText(progressText, "Updating Caravansary...");
+
+
+            bool deleteSuccess = await Task.Run(() => DeleteMainApplication());
+            if (!deleteSuccess)
+            {
+                this.progressText.Text = "Couldn't delate app";
+                retryButton.Visible = true;
+                closeButton.Visible = true;
+                return false;
+            }
+
+            bool downloadSuccess = await Task.Run(() => DownloadNewVersion());
+            if (!downloadSuccess)
+            {
+                this.progressText.Text = "Couldn't download app";
+                retryButton.Visible = true;
+                closeButton.Visible = true;
+                return false;
+            }
+
+            bool extractSuccess = await Task.Run(() => ExtractZip());
+            if (!extractSuccess)
+            {
+                this.progressText.Text = "Couldn't extract app";
+                retryButton.Visible = true;
+                closeButton.Visible = true;
+                return false;
+            }
+
+            UpdateLabelText(progressText, "");
+            UpdateLabelText(topLabel, "Finished updating...");
+
+            return true;
+        }
+
+        private async void retryButton_Click(object sender, EventArgs e)
+        {
+
+            retryButton.Visible = false;
+            closeButton.Visible = false;
+            await UpdateMainApp();
+            returnedArgs.Add("DownloadedEXEFromLauncherToAppdata");
+            StartMainApp();
+
+
+        }
+
+        private void closeButton_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void UpdateLabelText(Label label, string textToUpdate)
+        {
+            label.Invoke((MethodInvoker)delegate { label.Text = textToUpdate; });
+
+        }
+
     }
 }
